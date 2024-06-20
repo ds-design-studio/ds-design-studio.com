66f59a7027d421e1148f9d1e793ad56af0207f27
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2024-06-20 15:33:20 -0700

    Update /public

--- src/pages/api/og.ts
+++ src/pages/api/og.ts
@@ -1,13 +1,26 @@
 import { ImageResponse } from "@vercel/og";
 import type { APIRoute } from "astro";
 
+const defaultTitle = "DS Design Studio";
+const defaultDescription =
+  "DS Design is the independent studio of web designer and developer Daniel Saunders.";
+
+export const prerender = false;
+
 export const config = {
   runtime: "edge",
 };
 
-export async function GET() {
-  // const fontData = await fetch(
-  //   "https://ds-design-studio.com/fonts/Body-Grotesque-Fit-Extrabold.ttf"
+export async function GET({ request }: { request: Request }) {
+  const { searchParams } = new URL(request.url);
+  const hasTitle = searchParams.has("title");
+  const title = hasTitle ? searchParams.get("title") : defaultTitle;
+
+  const headerFontData = await fetch(
+    "https://ds-design-studio.com/fonts/Body-Grotesque-Fit-Extrabold.ttf"
+  ).then((res) => res.arrayBuffer());
+  // const bodyFontData = await fetch(
+  //   "https://ds-design-studio.com/fonts/Body-Text-Fit-Regular.ttf"
   // ).then((res) => res.arrayBuffer());
   // Astro doesn't support tsx endpoints so usign React-element objects
   const html = {
@@ -44,7 +57,19 @@ export async function GET() {
               fontSize: "48px",
               fontFamily: "Body Grotesque Fit",
             },
-            children: "My title here",
+            children: title,
+          },
+        },
+        {
+          type: "div",
+          props: {
+            style: {
+              fontSize: "20px",
+              maxWidth: "600px",
+              marginTop: "2rem",
+              // fontFamily: "Body Text Fit",
+            },
+            children: defaultDescription,
           },
         },
       ],
@@ -54,13 +79,19 @@ export async function GET() {
   return new ImageResponse(html, {
     width: 1200,
     height: 630,
-    // fonts: [
-    //   {
-    //     name: "Body Grotesque Fit",
-    //     data: fontData,
-    //     style: "normal",
-    //     weight: 800,
-    //   },
-    // ],
+    fonts: [
+      {
+        name: "Body Grotesque Fit",
+        data: headerFontData,
+        style: "normal",
+        weight: 800,
+      },
+      // {
+      //   name: "Body Text Fit",
+      //   data: bodyFontData,
+      //   style: "normal",
+      //   weight: 400,
+      // },
+    ],
   });
 }
