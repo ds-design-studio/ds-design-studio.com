a2e4a623db971e2250d58af4def14937803c0f3f
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2024-06-20 15:11:14 -0700

    vercel/og

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
 import browserslist from "browserslist";
 import { browserslistToTargets } from "lightningcss";
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -27,4 +27,6 @@ export default defineConfig({
   },
   // output: "hybrid",
   integrations: [mdx(), sitemap()],
+  output: "hybrid",
+  adapter: vercel(),
 });
---
+++ src/pages/api/og.ts
@@ -0,0 +1,66 @@
+import { ImageResponse } from "@vercel/og";
+import type { APIRoute } from "astro";
+
+export const config = {
+  runtime: "edge",
+};
+
+export async function GET() {
+  const fontData = await fetch(
+    "https://ds-design-studio.com/fonts/Body-Grotesque-Fit-Extrabold.ttf"
+  ).then((res) => res.arrayBuffer());
+  // Astro doesn't support tsx endpoints so usign React-element objects
+  const html = {
+    type: "div",
+    props: {
+      style: {
+        display: "flex",
+        flexDirection: "column",
+        background: "linear-gradient(to bottom right, #0a0c10, #303d50)",
+        color: "#f5f7f9",
+        textAlign: "center",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%",
+        width: "100%",
+      },
+      children: [
+        {
+          type: "img",
+          props: {
+            src: "https://ds-design-studio.com/svg/web-logo.svg",
+            width: 60,
+            height: 60,
+            style: {
+              position: "absolute",
+              top: "3rem",
+            },
+          },
+        },
+        {
+          type: "div",
+          props: {
+            style: {
+              fontSize: "48px",
+              fontFamily: "Body Grotesque Fit",
+            },
+            children: "My title here",
+          },
+        },
+      ],
+    },
+  };
+  // return new Response("hello world");
+  return new ImageResponse(html, {
+    width: 1200,
+    height: 630,
+    fonts: [
+      {
+        name: "Body Grotesque Fit",
+        data: fontData,
+        style: "normal",
+        weight: 800,
+      },
+    ],
+  });
+}
