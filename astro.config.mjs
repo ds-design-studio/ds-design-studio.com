185539c0ceb2505508096a5a71e5acf96f61ddd3
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2024-11-09 14:43:20 -0800

    Switch Vercel to Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import browserslist from "browserslist";
 import { browserslistToTargets } from "lightningcss";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
@@ -25,8 +27,7 @@ export default defineConfig({
     "/site-benefits": "/services/frontend-development",
     "/about-us": "/about",
   },
-  // output: "hybrid",
   integrations: [mdx(), sitemap()],
   output: "hybrid",
-  adapter: vercel(),
+  adapter: netlify(),
 });
